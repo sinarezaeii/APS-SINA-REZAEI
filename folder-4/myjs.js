f5a38b671f865ee0de6ff05ae6b560765165994d
sinarezaeii <sina.cod6610@gmail.com>
2022-01-16 09:55:43 -0800

    new

---
+++ folder-4/myjs.js
@@ -0,0 +1,55 @@
+// var Name = "Sina Rezai";
+// var name = "samaneh";
+// var NAME = "SINA";
+
+// alert("Hello " + Name);
+
+// let Nam = "Sina Rezai";
+// let nam = "samaneh";
+// let NAM = "SINA";
+
+// alert("help " + Nam);
+
+// const Nam = "Sina Rezai";
+// const nam = "samaneh";
+// const NAM = "SINA";
+
+// alert("help me " + Nam);
+
+// #console run
+// var Nam = "Sina Rezai";
+// const nam = "samaneh";
+// let NAM = "SINA";
+// console.log ("aps help me " + nam);
+
+// let x = 5;
+
+// if (x === 5) {
+// //   let x = 2 ;
+//   let x1 = 7 ;
+
+//   console.log(x1);
+
+// }
+
+
+// console.log(x);
+
+// // let x = 3;
+// let x = 5;
+
+// if (x === 5) {
+//   let x = 2 + 3;
+//   let x1 = 7 - 1;
+
+//   console.log(x+x1);
+
+// }
+
+// if (x === 3) {
+//     let x = 4 * 2;
+
+//     console.log(x);
+// }
+
+// console.log(x);
