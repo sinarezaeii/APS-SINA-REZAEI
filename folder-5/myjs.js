ef0bf428858f4871ee940efaf5c744e1c46da770
sinarezaeii <sina.cod6610@gmail.com>
2022-01-17 12:51:30 -0800

    new

---
+++ folder-5/myjs.js
@@ -0,0 +1,112 @@
+// // let myAge = 19.000000;
+// // let myAge = 19;
+// // let myAge= 10;
+// // let myAge=100;
+//  if (myAge >= 18 && myAge <= 25){
+//      document.write("sina");
+//  } else{
+//      document.write("samaneh");
+//  }
+// var a,b,c;
+
+// a = 12;
+
+// b = 8;
+
+//c= a + b;
+
+//c =a - b;
+
+//c = a * b;
+
+
+ //c = a / b ;
+
+//  c=a%b;
+
+// document.write(c);
+
+//console.log(Name);
+
+//window.alert(Name);
+
+// var number=15; //Number
+
+// var number2=15;
+
+// var Name="sina"; //String
+
+
+// var Variable= " aps "+23; //Multiple Variable
+
+// var Variable2=  45 - 23 + " aps";
+
+// var Variable3= "aps " + 45 + 23;
+
+// var Person={firstName:"sina",lastName:"rezaei"};//Object
+
+// var isComplete=false; //Boolean 
+
+// var isEqual; 
+
+// isEqual = (number==number2);
+
+
+// document.write(isEqual);
+
+// var names=new Array("mohammad","sina","milad","arad");
+
+
+// var MyArray=["ali",23,true,"hossein",24.543656456];
+
+
+// var colors = ["red","black","yellow","orange"];
+
+
+// //colors.reverse();
+
+// //colors.shift();
+
+// //colors.unshift("white");
+
+// //colors.pop();
+
+// //colors.push("white");
+
+// //var NewColors=colors.slice();
+
+// //var result=colors.indexOf("blue",0);
+
+// var arrayString=colors.join("   ");
+
+// console.log(arrayString);
+
+// let name = "sina";
+// let color = "Blue";
+// let language = "English";
+
+// if (name === "sina") {
+//   if (language === "English") {
+//     if (color === "Blue") {
+//       console.log("You are just like me");
+//     }
+//   }
+// }else{
+//   console.log("You have a diffrent name");
+// }
+
+// let spend = 100;
+
+// if (spend >= 20) {
+//   if (spend >= 50) {
+//     if (spend >= 100) {
+//       console.log("Shipping is free!");
+//     } else {
+//       console.log("Discounted shipping is $5");
+//     }
+//   } else {
+//     console.log("Discounted shipping is $7.50");
+//   }
+// } else {
+//   console.log("Shipping is $10");
+// }
